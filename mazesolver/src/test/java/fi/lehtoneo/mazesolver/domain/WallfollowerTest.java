92d036132f1d3d4fab0075ec4d41afc47d85e227
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-03-26 09:37:11 +0200

    more tests

---
+++ mazesolver/src/test/java/fi/lehtoneo/mazesolver/domain/WallfollowerTest.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.lehtoneo.mazesolver.domain;
+
+import fi.lehtoneo.mazesolver.util.Cell;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author ossij
+ */
+
+public class WallfollowerTest {
+    Maze maze1;
+    int[] start1 = new int[2];
+    int[] end1 = new int[2];
+    List<Cell> routeList1;
+    
+    Maze maze2;
+    int[] start2 = new int[2];
+    int[] end2 = new int[2];
+    List<Cell> routeList2;
+    
+    
+    
+    @Before
+    public void setUp() {
+        char[][] c =   {{'#','#','#','#', '#',},
+                        {'#','.','#','#', '#',},
+                        {'#','.','#','#', '#',},
+                        {'#','.','#','.', '#',},
+                        {'#','.','.','.', '#',},
+                        {'#','#','#','.', '#',},
+                        {'#','#','#','#', '#',}};
+        
+        maze1 = new Maze(c);
+        start1[0] = 1;
+        start1[1] = 1;
+        end1[0] = 5;
+        end1[1] = 3;
+        
+        Wallfollower wf1 = new Wallfollower(maze1.getGrid(), start1, end1);
+        wf1.solve();
+        routeList1 = wf1.getRouteList();
+                
+        Prim p = new Prim(21, 21);
+        
+        p.generate();
+        for(int i = 0; i < 21; i++) {
+            for(int j = 1; j < 3; j++) {
+            if(p.getGrid()[i][j] == '.') {
+                start2[0] = i;
+                start2[1] = j;
+                break;
+            }
+            }
+                
+        }
+        
+        for(int i = 0; i < 21; i++) {
+            for(int j = 10; j < 15; j++) {
+            if(p.getGrid()[i][j] == '.') {
+                end2[0] = i;
+                end2[1] = j;
+                break;
+            }
+            }
+                
+        }
+        
+        maze2 = new Maze(p.getGrid());
+        
+        Wallfollower wf2 = new Wallfollower(maze2.getGrid(), start2, end2);
+        wf2.solve();
+        routeList2 = wf2.getRouteList();
+    }
+    
+    @Test
+    public void InSimpleMazewFFindsSomePath() {
+        
+        assertEquals(routeList1.size( ), 7);
+        
+    }
+    
+    @Test
+    public void InSimpleMzewFPathIsExpected() {
+        
+        List<Cell> routeShouldBe = new ArrayList<>();
+        routeShouldBe.add(new Cell(1, 1));
+        routeShouldBe.add(new Cell(2, 1));
+        routeShouldBe.add(new Cell(3, 1));
+        routeShouldBe.add(new Cell(4, 1));
+        routeShouldBe.add(new Cell(4, 2));
+        routeShouldBe.add(new Cell(4, 3));
+        routeShouldBe.add(new Cell(5, 3));
+        
+        for(int i = 0; i < routeList1.size(); i++) {
+            assertTrue(routeList1.get(i).equals(routeShouldBe.get(i)));
+        }
+    }
+
+    @Test 
+    public void InRandomMazeLastCellInRoutelistIsTarget() {
+        
+        
+        assertEquals(routeList2.get(routeList2.size()-1).getRow(), end2[0]);
+        assertEquals(routeList2.get(routeList2.size()-1).getColumn(), end2[1]);
+        
+    }
+    
+    
+    @Test 
+    public void InRandomMazeAllOfTheCellsInRouteListArePath() {
+        
+        char h = '.';
+        
+        for (Cell c: routeList2) {
+            assertEquals(maze2.getGrid()[c.getRow()][c.getColumn()], h);
+        }
+        
+    }
+    
+    
+    @Test
+    public void InRandomMazeThePathCanBeTaken() {
+        
+        for (int i = 0; i < routeList2.size() - 1; i++) {
+            Cell curr = routeList2.get(i);
+            int currR = curr.getRow();
+            int currC = curr.getColumn();
+            Cell next = routeList2.get(i+1);
+            int nextR = next.getRow();
+            int nextC = next.getColumn();
+            
+            if((currR + 1 == nextR && currC == nextC) || (currR - 1 == nextR && currC == nextC)) {
+                assertTrue(true);
+            } else if ((currR == nextR && currC == nextC + 1) || (currR == nextR && currC == nextC - 1)) {
+                assertTrue(true);
+            } else {
+                assertTrue(false);
+            }
+        }
+    }
+        
+    
+}
---
+++ mazesolver/src/test/java/fi/lehtoneo/mazesolver/util/LinkedListTest.java
@@ -0,0 +1,124 @@
+package fi.lehtoneo.mazesolver.util;
+
+import fi.lehtoneo.mazesolver.util.LinkedList;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import org.junit.Before;
+
+
+public class LinkedListTest {
+    
+    
+    
+    @Test
+    public void sizeIncreasesWhenAddingObjects() {
+        LinkedList<Integer> testList = new LinkedList<>();
+        
+        testList.add(1);
+        testList.add(3);
+        
+        assertEquals(testList.size(), 2);
+    }
+    
+    @Test
+    public void getReturnsCorrectObjectFromMiddleOfList() {
+        LinkedList<Integer> testList = new LinkedList<>();
+        
+        testList.add(1);
+        testList.add(3);
+        testList.add(9);
+        int shouldBe = testList.get(1);
+        
+        assertEquals(shouldBe, 3);
+        
+    }
+    
+    @Test
+    public void getReturnsCorrectObjectFromBeginningOfList() {
+        LinkedList<Integer> testList = new LinkedList<>();
+        
+        testList.add(1);
+        testList.add(3);
+        testList.add(9);
+        int shouldBe = testList.get(0);
+        
+        assertEquals(shouldBe, 1);
+        
+    }
+
+    
+    
+    @Test
+    public void getReturnsCorrectObjectFromEndOfList() {
+        LinkedList<Integer> testList = new LinkedList<>();
+        
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        testList.add(5);
+        testList.add(6);
+        
+        int shouldBe = testList.get(5);
+        
+        assertEquals(shouldBe, 6);
+        
+    }
+    
+    @Test
+    public void removingAnObjectInTheMiddleRemovesObject() {
+        LinkedList<Integer> testList = new LinkedList<>();
+        
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        testList.add(5);
+        testList.add(6);
+        
+        testList.remove(3);
+        int shouldBe = testList.get(3);
+        assertEquals(shouldBe, 5);
+        assertEquals(testList.size(), 5);
+    }
+    
+    @Test
+    public void removingLastRemovesObject() {
+        LinkedList<Integer> testList = new LinkedList<>();
+        
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        testList.add(5);
+        testList.add(6);
+        
+        testList.remove(testList.size() - 1);
+        int shouldBe = testList.get(testList.size() - 1);
+        assertEquals(shouldBe, 5);
+        assertEquals(testList.size(), 5);
+    }
+    
+    @Test
+    public void removingFirstRemovesObject() {
+        LinkedList<Integer> testList = new LinkedList<>();
+        
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        testList.add(5);
+        testList.add(6);
+        
+        testList.remove(0);
+        int shouldBe = testList.get(0);
+        
+        assertEquals(shouldBe, 2);
+        assertEquals(testList.size(), 5);
+    }
+        
+
+    
+    
+}
